046f11d85513c84bd29d43365ef322ed1566dbe8
Astitva Veer Garg <av4764@srmist.edu.in>
2022-03-18 01:55:46 +0530

    Done

--- Api.py
+++ Api.py
@@ -46,7 +46,7 @@ def Predicted(symptomsgg):
     for x in range(0,len(l1)):
         l2.append(0)
 
-    dataset=pd.read_csv(r"/dataset/Training.csv")
+    dataset=pd.read_csv(r"./Dataset/Training.csv")
 
     dataset.replace({'prognosis':{'Fungal infection':0,'Allergy':1,'GERD':2,'Chronic cholestasis':3,'Drug Reaction':4,
     'Peptic ulcer diseae':5,'AIDS':6,'Diabetes ':7,'Gastroenteritis':8,'Bronchial Asthma':9,'Hypertension ':10,
@@ -64,7 +64,7 @@ def Predicted(symptomsgg):
     y = dataset[["prognosis"]]
     np.ravel(y)
 
-    tr=pd.read_csv(r"/dataset/Testing.csv")
+    tr=pd.read_csv(r"./Dataset/Testing.csv")
     tr.replace({'prognosis':{'Fungal infection':0,'Allergy':1,'GERD':2,'Chronic cholestasis':3,'Drug Reaction':4,
     'Peptic ulcer diseae':5,'AIDS':6,'Diabetes ':7,'Gastroenteritis':8,'Bronchial Asthma':9,'Hypertension ':10,
     'Migraine':11,'Cervical spondylosis':12,'Paralysis (brain hemorrhage)':13,'Jaundice':14,'Malaria':15,'Chicken pox':16,'Dengue':17,'Typhoid':18,'hepatitis A':19,'Hepatitis B':20,'Hepatitis C':21,'Hepatitis D':22,'Hepatitis E':23,'Alcoholic hepatitis':24,'Tuberculosis':25,'Common Cold':26,'Pneumonia':27,'Dimorphic hemmorhoids(piles)':28,'Heart attack':29,'Varicose veins':30, 'Hypothyroidism':31,'Hyperthyroidism':32,'Hypoglycemia':33,'Osteoarthristis':34,'Arthritis':35,'(vertigo) Paroymsal  Positional Vertigo':36,'Acne':37,'Urinary tract infection':38,'Psoriasis':39,'Impetigo':40}},inplace=True)
